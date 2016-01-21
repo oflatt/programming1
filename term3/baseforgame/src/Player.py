40ca42dad73c2adf5250e166b9d80d2256769a9b
Oliver Flatt <oflatt@gmail.com>
2016-01-21 06:40:07 -0700

    Changed movement
    More fluid, keeps track of keys kept down and when they were pressed

--- term3/baseforgame/src/Player.py
+++ term3/baseforgame/src/Player.py
@@ -4,6 +4,10 @@ import pygame, variables
 class Player():
     xspeed = 0
     yspeed = 0
+    leftpresstime = 0
+    rightpresstime = 0
+    uppresstime = 0
+    downpresstime = 0
 
     def __init__(self, xpos, ypos):
         self.xpos = xpos
@@ -14,19 +18,43 @@ class Player():
 
     def keypress(self, k):
         if k == pygame.K_LEFT:
+            self.leftpresstime = pygame.time.get_ticks()
             self.xspeed = -3
         elif k == pygame.K_RIGHT:
+            self.rightpresstime = pygame.time.get_ticks()
             self.xspeed = 3
         elif k == pygame.K_UP:
+            self.uppresstime = pygame.time.get_ticks()
             self.yspeed = -3
         elif k == pygame.K_DOWN:
+            self.downpresstime = pygame.time.get_ticks()
             self.yspeed = 3
 
     def keyrelease(self, k):
-        if k == pygame.K_LEFT or k == pygame.K_RIGHT:
-            self.xspeed = 0
-        elif k == pygame.K_UP or k == pygame.K_DOWN:
-            self.yspeed = 0
+        if k == pygame.K_LEFT:
+            self.leftpresstime = 0
+            if self.rightpresstime == 0:
+                self.xspeed = 0
+            else:
+                self.xspeed = 3
+        elif k == pygame.K_RIGHT:
+            self.rightpresstime = 0
+            if self.leftpresstime == 0:
+                self.xspeed = 0
+            else:
+                self.xspeed = -3
+        elif k == pygame.K_UP:
+            self.uppresstime = 0
+            if self.downpresstime == 0:
+                self.yspeed = 0
+            else:
+                self.yspeed = 3
+        elif k == pygame.K_DOWN:
+            self.downpresstime = 0
+            if self.uppresstime == 0:
+                self.yspeed = 0
+            else:
+                self.yspeed = 3
 
     def move(self):
         self.xpos += self.xspeed
